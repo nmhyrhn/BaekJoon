c4e7c82645abd516716cb737031e39da821c1fad
nmhyrhn <gbs06193@gmail.com>
2025-10-03 02:11:51 +0900

    1003 update

---
+++ A/Q10757.java
@@ -0,0 +1,20 @@
+package BaekJoon.A;
+
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Q10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
+
+        System.out.print(a.add(b));
+
+    }
+}
