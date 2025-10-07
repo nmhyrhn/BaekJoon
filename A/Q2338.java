477aa893c8967eecfdf026ad01b796f195ed9656
nmhyrhn <gbs06193@gmail.com>
2025-10-07 17:51:48 +0900

    1007 update

---
+++ A/Q2338.java
@@ -0,0 +1,15 @@
+package BaekJoon.A;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Q2338 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.print(a.multiply(b));
+    }
+}
