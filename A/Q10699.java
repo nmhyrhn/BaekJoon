fc34f134acf1a059d136a202ca70f7353a5aad55
nmhyrhn <gbs06193@gmail.com>
2025-10-12 16:26:41 +0900

    1012 update

---
+++ A/Q10699.java
@@ -0,0 +1,15 @@
+package BaekJoon.A;
+
+import java.time.*;
+
+public class Q10699 {
+
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+
+
+    }
+
+}
