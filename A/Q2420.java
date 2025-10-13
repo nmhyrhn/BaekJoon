b391634c6ac6a835a9105abda67e1fb58c878217
nmhyrhn <gbs06193@gmail.com>
2025-10-13 09:27:50 +0900

    1013 update

---
+++ A/Q2420.java
@@ -0,0 +1,14 @@
+package BaekJoon.A;
+
+import java.util.Scanner;
+
+public class Q2420 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+
+        System.out.println(Math.abs(a-b));
+    }
+}
