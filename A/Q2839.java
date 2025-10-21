14404c38cf89f5764ca790a433a8168095ea93cf
nmhyrhn <gbs06193@gmail.com>
2025-10-21 10:23:45 +0900

    1021 update

---
+++ A/Q2839.java
@@ -0,0 +1,21 @@
+package BaekJoon.A;
+
+import java.util.*;
+
+public class Q2839 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n ==4 || n == 7) {
+            System.out.print(-1);
+        } else if (n%5 ==0) {
+            System.out.print(n/5);
+        } else if (n%5 == 1 || n%5 == 3) {
+            System.out.print((n/5)+1);
+        } else {
+            System.out.print((n/5)+2);
+        }
+
+    }
+}
